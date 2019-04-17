a02f8633eaa701d78d676532267c30107b5f7b82
TDHTTT <tdhttt@hotmail.com>
2019-04-16 22:39:33 -0700

    out of 162 e, 2 of them falls too far

--- finde.C
+++ finde.C
@@ -4,9 +4,34 @@ R__LOAD_LIBRARY(libDelphes)
 #include "external/ExRootAnalysis/ExRootTreeReader.h"
 #include <vector>
 #include <algorithm>
+#include <math.h>
 #endif
 
+// didn't find good tcl lib
 //------------------------------------------------------------------------------
+
+void printV(std::vector<float> v)
+{
+    std::cout << "length=" << v.size() << ": ";
+    for(int i=0; i<v.size(); ++i)
+    {
+        std::cout << v[i] << ' ';
+    }
+    std::cout << endl;
+}
+
+int findV(std::vector<float> v, float e)
+{
+    for(int i=0; i<v.size()-1; ++i)
+    {
+        // what if electron's eta/phi on edge?
+        if(e>v[i] && e<v[i+1])
+        {
+            return i;
+        }
+    }
+    return -1;
+}
 
 void finde(const char *inputFile)
 {
@@ -25,8 +50,8 @@ void finde(const char *inputFile)
   TClonesArray *branchHCal = treeReader->UseBranch("HCalTowers");
   TClonesArray *branchElectron = treeReader->UseBranch("Electron");
 
-  Electron *electron, *highe, *fe;
-  float curr_ePT = -999999.0;
+  Electron *electron, *highe;
+  //float curr_ePT = -999999.0;
   Tower *ECal;
   Tower *HCal;
 
@@ -36,50 +61,82 @@ void finde(const char *inputFile)
  
   std::vector<float> ephi, eeta;
   std::vector<float> hphi, heta;
+  // b = barrell
+  std::vector<float> bephi, beeta;
+  std::vector<float> bhphi, bheta;
+  int total_e = 0;
+  int far_e = 0;
+  // add pre-configured barrell cells
+  for(Int_t i=-100; i<=100; i++)
+  {
+    beeta.push_back(i*0.025);
+  }
+  for(Int_t i=-126; i<=126; i++)
+  {
+    bephi.push_back(i*(M_PI)/126);
+  }
+  for(Int_t i=-25; i<=25; i++)
+  {
+    bheta.push_back(i*0.1);
+  }
+  for(Int_t i=-31; i<=31; i++)
+  {
+    bhphi.push_back(i*(M_PI)/31);
+  }
+  printV(beeta);
+  printV(bephi);
+  printV(bheta);
+  printV(bhphi);
 
   // Loop over all events
   for(Int_t entry = 0; entry < numberOfEntries; ++entry)
+  //for(Int_t entry = 0; entry < 2; ++entry)
   {
     treeReader->ReadEntry(entry);
-    std::cout << "Event#" << entry << ": " << std::endl;
+    //std::cout << "Event#" << entry << ": " << std::endl;
     int enu = branchElectron->GetEntriesFast();
-    std::cout << "  " << enu << " e in total" << std::endl;
+    total_e += enu;
+    //std::cout << "  " << enu << " e in total" << std::endl;
 
-    fe = (Electron*) branchElectron->At(0);
+    highe = (Electron*) branchElectron->At(0);
     // Selecting the electron with the highest PT
     // Though it seems that the highe will be the 1st element in branchElectron
     for(Int_t i = 0; i < enu; ++i)
     {
       electron = (Electron*) branchElectron->At(i);
-      if (electron->PT >= fe->PT)
+      if (electron->PT >= highe->PT)
       {
-        curr_ePT = electron->PT;
+        //curr_ePT = electron->PT;
         highe = electron;
       }
-      std::cout << "  e#" << i << ", PT=" << electron->PT << ", currPT:" << curr_ePT << ", Eta: " << electron->Eta << ", Phi: " << electron->Phi << std::endl;
+      //std::cout << "  e#" << i << ", PT=" << electron->PT << ", currPT:" << electron->PT << ", Eta: " << electron->Eta << ", Phi: " << electron->Phi << std::endl;
+    }
+    int ii = findV(beeta,electron->Eta);
+    std::cout << "!!!" << ii << std::endl;
+    if(ii > beeta.size()-15)
+    {
+        far_e += 1;
+        std::cout << "The electron falls too far!!" << std::endl;
     }
 
     // For events with at least one electron
     if (enu > 0)
     {
-      //std::cout << "Event#: " << entry << std::endl;
-      int enu = branchElectron->GetEntriesFast();
-      //std::cout << "  e#: " << enu << std::endl;
       for(Int_t j = 0; j < branchECal->GetEntriesFast(); ++j)
       {
         ECal = (Tower*) branchECal->At(j);
-        std::cout << "    Ecal#" << j << ", Ecal.ET: " << ECal->ET << ", Ecal.E: " << ECal->E  <<  std::endl;
-        for (Int_t ii = 0 ; ii < 4; ++ii)
-        {
-          std::cout << "      Edge#" << ii << ": " << ECal->Edges[ii];
-          
-        }
-        std::cout << std::endl;
-        std::cout <<  "      Random: " << "Ecal.Eta: " << ECal->Eta << ", Ecal.Phi: " << ECal->Phi << std::endl;
+        //std::cout << "    Ecal#" << j << ", Ecal.ET: " << ECal->ET << ", Ecal.E: " << ECal->E  <<  std::endl;
+       // for (Int_t ii = 0 ; ii < 4; ++ii)
+       // {
+       //   std::cout << "      Edge#" << ii << ": " << ECal->Edges[ii];
+       //   
+       // }
+       // std::cout << std::endl;
+       // std::cout <<  "      Random: " << "Ecal.Eta: " << ECal->Eta << ", Ecal.Phi: " << ECal->Phi << std::endl;
 
         float eceta = 0.5*(ECal->Edges[0]+ECal->Edges[1]);
         float ecphi = 0.5*(ECal->Edges[2]+ECal->Edges[3]); 
-        std::cout <<  "      Center: " << "Ecal.Eta: " << eceta  << ", Ecal.Phi: " << ecphi << std::endl;
+        //std::cout <<  "      Center: " << "Ecal.Eta: " << eceta  << ", Ecal.Phi: " << ecphi << std::endl;
         //if (ECal->E == highe->PT)
         //{
         //  std::cout << "!!!!!!" << j << std::endl; 
@@ -108,6 +165,8 @@ void finde(const char *inputFile)
       }
     }
   }
+    
+    std::cout << "Total: " << total_e << ", Far: " << far_e << std::endl;
     
     //Stats for min max in histogram
     float maxephi = *std::max_element(std::begin(ephi), std::end(ephi));
