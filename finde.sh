3a5785af0354d44966599b9fb6856ed337153931
TDHTTT <tdhttt@hotmail.com>
2019-04-28 23:12:15 -0700

    finde testing

--- finde.sh
+++ finde.sh
@@ -1,3 +1,6 @@
-root -l ./finde.C'("/c/Users/tdhttt/workspace/hep/expmpdg/ppZee/Roots/delphes_1.root")'
-mkdir -p ./png
-mv *.png ./png
+#!/usr/bin/env bash
+source ./init.sh
+root -l ./src/finde.C'("'$1'")'
+mkdir -p ./output/png ./output/csv
+mv *.png ./output/png
+mv *.csv ./output/csv
